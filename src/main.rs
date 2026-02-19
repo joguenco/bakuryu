95647d0dac46b1630d40d960311467c91a4b0dce
Jorge Luis <jorgeluis@resolvedor.dev>
2026-02-18 20:10:25 -0500

    [ADD] Logger in terminal

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ use std::time::Duration;
 use actix_multipart::form::MultipartFormConfig;
 use actix_web::{App, HttpServer, middleware::Logger, web};
 use actix_web_httpauth::middleware::HttpAuthentication;
+use env_logger::Env;
 
 pub type DBPool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
@@ -24,13 +25,13 @@ pub type DBPool = r2d2::Pool<ConnectionManager<PgConnection>>;
 async fn main() -> std::io::Result<()> {
     let con = connect();
     let pool_data = web::Data::new(con);
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
 
     HttpServer::new(move || {
         let auth = HttpAuthentication::with_fn(auth_validator);
         App::new()
             .app_data(pool_data.clone())
             .app_data(web::PayloadConfig::new(2048 * 1024 * 1024))
-            .wrap(Logger::default())
             .service(ping_handler)
             .service(
                 web::scope("")
@@ -39,6 +40,7 @@ async fn main() -> std::io::Result<()> {
                     .service(backup_handler),
             )
             .app_data(MultipartFormConfig::default().total_limit(2048 * 1024 * 1024))
+            .wrap(Logger::default())
     })
     .client_request_timeout(Duration::from_secs(180))
     .bind(("127.0.0.1", 8080))?
