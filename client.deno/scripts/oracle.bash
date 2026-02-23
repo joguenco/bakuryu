6c08180f9cef41d85b4304ae03bec72aed0aea53
Jorge Luis <jorgeluis@resolvedor.dev>
2026-02-22 21:57:00 -0500

    [REF] Rename client to client.deno

---
+++ client.deno/scripts/oracle.bash
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+date_now=$(date +"%Y-%m-%d_%H-%M")
+
+su - oracle -c "expdp system/MySecret0 schemas=demo,demo_data DIRECTORY=backup DUMPFILE=demo.$date_now.dmp LOGFILE=backup.log"
+su - oracle -c "7za a /home/oracle/backup/demo.$date_now.dmp.7z /home/oracle/backup/demo.$date_now.dmp"
